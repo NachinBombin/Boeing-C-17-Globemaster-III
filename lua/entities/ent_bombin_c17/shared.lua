c144774482ba4fc0f5925b112ff563988ff79638
NachinBombin <solidnachox@gmail.com>
2026-05-09 02:04:07 -0400

    feat: add ent_bombin_c17 - C-17 Globemaster III full re-skin of B-52
    - Entity: ent_bombin_c17 (model: models/custom/c17_anim_compliment.mdl)
    - All flight, weapon, damage, orbit, tumble systems copied from B-52
    - 4-engine contrail system (C-17 wing pod offsets)
    - Damage tier FX (3 tiers, net bombin_c17_damage_tier)
    - NPC call system (combine callers, flare throw, ConVars: npc_bombinc17_*)
    - Client tool menu under Bombin Support tab
    - Sound path: sound/c17/c17.wav (drop-in replacement)

---
+++ lua/entities/ent_bombin_c17/shared.lua
@@ -0,0 +1,6 @@
+ENT.Type           = "anim"
+ENT.Base           = "base_gmodentity"
+ENT.PrintName      = "Bombin C-17 Globemaster III"
+ENT.Author         = "NachinBombin"
+ENT.Spawnable      = false
+ENT.AdminSpawnable = false
